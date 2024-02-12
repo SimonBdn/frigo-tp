bbe97078ef05e2838694f8f0586d5f03736a7796
SimonBdn <simbo21072002@gmail.com>
2024-02-12 12:15:34 +0100

    Get Ali

--- frigotp/src/components/AlimentAffich.vue
+++ frigotp/src/components/AlimentAffich.vue
@@ -1,6 +1,7 @@
 <script setup>
 
 import {onMounted} from "vue";
+import Aliment from "@/Aliment";
 
 const url="https://webmmi.iut-tlse3.fr/~pecatte/frigo/public/7/produits";
 const listeAli = reactive([]);
@@ -16,7 +17,7 @@ function searchAli() {
       let aliments = dataJSON.results
       listeAli.splice(0,listeAli.length)// vider la liste
       for(let aliment of aliments){
-        listeAli.push(new Aliment(Aliment))
+        listeAli.push(new Aliment(aliment.id, aliment.nom, aliment.qte, aliment.photo))
       }
     })
     .catch((error) => {
