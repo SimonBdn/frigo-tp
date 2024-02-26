41839ccd688312734fc433eb6e71a6bfe29a60b1
SimonBdn <simbo21072002@gmail.com>
2024-02-26 13:59:54 +0100

    mise en page en cours

--- frigotp/src/pages/index.vue
+++ frigotp/src/pages/index.vue
@@ -1,7 +1,24 @@
 <template>
-  <HelloWorld />
+  <v-container class="frig">
+    <HelloWorld />
+  </v-container>
+
+
 </template>
 
 <script setup>
-  //
+
 </script>
+
+<style scoped>
+.frig{
+  background-image: url("../assets/Frigo.png");
+  background-size: cover;
+  background-position: center;
+  padding: 40px;
+  min-height: 600px;
+}
+
+</style>
+
+
